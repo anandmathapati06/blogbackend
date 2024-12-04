8cbb77832e1307703bb40477bbc5de57ea6c60db
Anand Mathapati <0606dnana@gmail.com>
2024-12-04 16:35:26 +0530

    ex

--- Dbconnection.js
+++ Dbconnection.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
 
 
-dotenv.config()
-async function Dbconnection()
 
-{
-    const mongouri = process.env.MONGO_URI
-    const connection = await mongoose.connect(mongouri).then(()=>{
+
+async function Dbconnection(){
+    // const mongouri = process.env.MONGO_URI
+    const connection = await mongoose.connect("mongodb+srv://Anand:Anand123@anand.f2ix40i.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Anand").then(()=>{
         console.log("DB connected")
         }).catch(()=>{
             console.log("Error while connecting to mongo DB");
