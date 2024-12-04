4d8b5d5202c22239ff753eebed021e28363ff15a
Anand Mathapati <0606dnana@gmail.com>
2024-12-04 16:08:37 +0530

    q

--- Dbconnection.js
+++ Dbconnection.js
@@ -9,6 +9,9 @@ async function Dbconnection()
     const mongouri = process.env.MONGO_URI
     const connection = await mongoose.connect(mongouri).then(()=>{
         console.log("DB connected")
+        }).catch(()=>{
+            console.log("Error while connecting to mongo DB");
+            
         })
 
     
