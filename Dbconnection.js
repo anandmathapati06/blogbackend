511caf6d861b4a3da5642c708c5384a429fa10f0
Anand Mathapati <0606dnana@gmail.com>
2024-12-04 16:59:49 +0530

    go

--- Dbconnection.js
+++ Dbconnection.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-
-
+import dotenv from 'dotenv'
 
 
 async function Dbconnection(){
-    const mongouri = process.env.MONGO_URI
-    const connection = await mongoose.connect(mongouri).then(()=>{
+    
+    dotenv.config()
+    const connection = await mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("DB connected")
         }).catch(()=>{
             console.log("Error while connecting to mongo DB");
--- index.js
+++ index.js
@@ -10,11 +10,7 @@ app.use(cors({ origin: '*' }))
 app.use(express.json({limit : '5mb'}))
 dotenv.config()
 
-// app.use(express.static("./frontend/blogsite/dist"))
 
-// app.get("*", (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-// })
 
 app.get("/",open)
 app.post("/posts",createBlog)
