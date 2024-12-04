3dc76b972f0262b55a8d2403798b99a18636b0eb
Anand Mathapati <0606dnana@gmail.com>
2024-12-04 16:56:19 +0530

    go

--- Dbconnection.js
+++ Dbconnection.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 
 
 async function Dbconnection(){
-    // const mongouri = process.env.MONGO_URI
-    const connection = await mongoose.connect("mongodb+srv://Anand:Anand123@anand.f2ix40i.mongodb.net/blogDB?retryWrites=true&w=majority&appName=Anand").then(()=>{
+    const mongouri = process.env.MONGO_URI
+    const connection = await mongoose.connect(mongouri).then(()=>{
         console.log("DB connected")
         }).catch(()=>{
             console.log("Error while connecting to mongo DB");
